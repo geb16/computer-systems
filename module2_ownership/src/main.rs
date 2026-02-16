15031cfaf4e0ebcf3711b3a36263c3696baa9820
Danyob16 <deg7@student.london.ac.uk>
2026-02-16 19:26:31 +0000

    Add initial implementation for module2_ownership with Cargo configuration and main logic

---
+++ module2_ownership/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut s = String::from("hi");
+
+    let r1 = &s; // & means immutable reference
+    let r2 = &s; // another immutable reference
+    //let r3 = &mut s; // <-- should fail why
+
+    println!("{r1} {r2} {r3}");
+}
